2b9a148ac72ed54857e4ed07b0ab1fa0b24f0ee8
bvw <bvw@traficon.com>
2013-01-22 17:41:50 +0100

    * Moved examples to other directory.

---
+++ books/Learning_OpenCV/Chapter03/examples/example3-01.cpp
@@ -0,0 +1,20 @@
+typedef struct CvMat {
+    int type;
+    int step;
+    int* refcount;     // for internal use only
+    union {
+         uchar* ptr;
+         short* s;
+         int*    i;
+         float* fl;
+         double* db;
+    } data;
+    union {
+         int rows;
+         int height;
+    };
+    union {
+         int cols;
+         int width;
+    };
+} CvMat;
