ef423c0b0c3d5595075b5a7bf894cd44785180f0
bvw <bvw@traficon.com>
2013-01-25 16:40:45 +0100

    * Added exercise 5-1b.

---
+++ books/Learning_OpenCV/Chapter05/exercises/exercise5-01b.cpp
@@ -0,0 +1,35 @@
+//#include <iostream.h>
+#include <cv.h>
+#include <highgui.h>
+
+int main( int argc, char* argv[] ) {
+
+    IplImage* src = cvLoadImage( argv[1] );
+    IplImage* dst1 = cvCreateImage( cvGetSize(src), src->depth, src->nChannels );
+    IplImage* dst2 = cvCreateImage( cvGetSize(src), src->depth, src->nChannels );
+
+    cvNamedWindow( "Original", CV_WINDOW_AUTOSIZE );
+    cvNamedWindow( "Twice with 5 by 5", CV_WINDOW_AUTOSIZE );
+    cvNamedWindow( "Once with 11 by 11", CV_WINDOW_AUTOSIZE );
+
+    cvSmooth( src, dst1, CV_GAUSSIAN, 5, 5 );
+    cvSmooth( dst1, dst1, CV_GAUSSIAN, 5, 5 );
+
+    cvSmooth( src, dst2, CV_GAUSSIAN, 11, 11 );
+
+    cvShowImage( "Original", src );
+    cvShowImage( "Twice with 5 by 5", dst1 );
+    cvShowImage( "Once with 11 by 11", dst2 );
+
+    cvWaitKey();
+
+    cvReleaseImage( &src );
+    cvReleaseImage( &dst1 );
+    cvReleaseImage( &dst2 );
+
+    cvDestroyWindow( "Original" );
+    cvDestroyWindow( "Twice with 5 by 5" );
+    cvDestroyWindow( "Once with 11 by 11" );
+
+    return 0;
+}
