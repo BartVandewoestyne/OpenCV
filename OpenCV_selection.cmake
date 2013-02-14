d24f02706d552b10cd44b4b45dd3a003f13b24ff
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-02-14 15:17:24 +0100

    Removed superfluous use of a variable.

--- OpenCV_selection.cmake
+++ OpenCV_selection.cmake
@@ -1,21 +1,19 @@
 # Set this to the version of OpenCV that you need.
-set(OPENCV_LIB "fat")
+set(OPENCV_LIB "latest")
 
 if(OPENCV_LIB STREQUAL "latest")
 
   message(STATUS "Using my own compiled OpenCV.")
-  set(OpenCV_INCLUDE_DIR "/opt/opencv/latest/include/opencv")
-  include_directories(${OpenCV_INCLUDE_DIR})
+  include_directories("/opt/opencv/latest/include/opencv")
+  link_directories("/opt/opencv/latest/lib")
   set(OpenCV_LIBS "opencv_gpu;opencv_contrib;opencv_legacy;opencv_objdetect;opencv_calib3d;opencv_features2d;opencv_video;opencv_highgui;opencv_ml;opencv_imgproc;opencv_flann;opencv_core")
-  link_directories(/opt/opencv/latest/lib)
 
 elseif(OPENCV_LIB STREQUAL "fat")
 
   message(STATUS "Using OpenCV from Traficon's fat repository.")
-  set(OpenCV_INCLUDE_DIR "$ENV{HOME}/Git/Research/fat/inc/TraficonOpenCv/opencv")
-  include_directories(${OpenCV_INCLUDE_DIR})
+  include_directories("$ENV{HOME}/Git/Research/fat/inc/TraficonOpenCv/opencv")
+  link_directories("$ENV{HOME}/Git/Research/fat/bin/TraficonOpenCv/linux_release" "$ENV{HOME}/Git/Research/fat/bin/FFMpeg/linux_release")
   set(OpenCV_LIBS "opencv_gpu;opencv_contrib;opencv_legacy;opencv_objdetect;opencv_calib3d;opencv_features2d;opencv_video;opencv_highgui;opencv_ml;opencv_imgproc;opencv_flann;opencv_core")
-  link_directories($ENV{HOME}/Git/Research/fat/bin/TraficonOpenCv/linux_release $ENV{HOME}/Git/Research/fat/bin/FFMpeg/linux_release)
 
 elseif(OPENCV_LIB STREQUAL "system")
 
