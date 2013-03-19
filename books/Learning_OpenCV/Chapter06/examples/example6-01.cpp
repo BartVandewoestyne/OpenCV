53090b66cd9c364c97aeb52e1534a6f3950b2605
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-03-19 11:52:16 +0100

    Minor change to example 6-01.

--- books/Learning_OpenCV/Chapter06/examples/example6-01.cpp
+++ books/Learning_OpenCV/Chapter06/examples/example6-01.cpp
@@ -28,6 +28,7 @@ int main(int argc, char** argv) {
       argv[1],
       CV_LOAD_IMAGE_GRAYSCALE
     );
+
     IplImage* src = cvLoadImage( argv[1] ); // Changed for prettier show in color.
     CvMemStorage* storage = cvCreateMemStorage(0);
     cvSmooth( image, image, CV_GAUSSIAN, 5, 5 );
@@ -38,6 +39,7 @@ int main(int argc, char** argv) {
       4, 
       image->width/10 
     ); 
+
     for ( int i = 0; i < results->total; i++ ) {
         float* p = (float*) cvGetSeqElem( results, i );
         CvPoint pt = cvPoint( cvRound( p[0] ), cvRound( p[1] ) );
